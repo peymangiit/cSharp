50efe92e69a9d04b62537530a22ceb59f43d58e5
Ali Aghajani <105543467+peymangiit@users.noreply.github.com>
2023-03-04 18:42:03 +0330

    git initiated

---
+++ NumbersAddition/NumbersAddition/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersAddition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //............................................................................
+            //introduction firstname and secondname hello c#
+            //............................................................................
+            string firstName, LastName;
+
+            Console.Write("please write first Name: ");
+            firstName = Console.ReadLine();
+
+            Console.Write("please write Last Name: ");
+            LastName = Console.ReadLine();
+
+            Console.WriteLine("------------------------------------------");
+            Console.Write("Hello ");
+            Console.Write(firstName);
+            Console.Write("  ");
+            Console.Write(LastName);
+            Console.WriteLine("  Welcome to C#");
+            Console.ReadKey();
+
+           
+ //............................................................................
+            //introduction sum two Numbers
+            //............................................................................
+
+
+           
+            //int firstNumber, secondNumber;
+
+            //Console.Write("please input first Number: ");
+            //firstNumber = int.Parse(Console.ReadLine());
+
+            //Console.Write("please input second Number: ");
+            //secondNumber = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("------------------------------------------");
+
+            //Console.WriteLine(firstNumber + secondNumber);
+
+
+
+           
+
+           
+            
+
+
+            //............................................................................
+            //a programe that takes year , mounths and days and show how many days you lived
+            //............................................................................
+            //int years, mounths, days, DaysAge;
+            
+            //Console.Write("please write your age in years  : ");
+            //years = int.Parse(Console.ReadLine())*365;
+
+            //Console.Write("please write how many mounths  : ");
+            //mounths = int.Parse(Console.ReadLine()) * 30;
+
+            //Console.Write("please write how many days  : ");
+            //days = int.Parse(Console.ReadLine()) ;
+
+            //Console.WriteLine("--------------------------------");
+            //DaysAge = years + mounths + days;
+
+            //Console.WriteLine("You lived for {0} days!",DaysAge);
+
+
+
+
+            //............................................................................
+            //a programe that takes single character and show ascii code of it
+            //............................................................................  
+            //Console.Write("please write a single character and press enter: ");
+            //int character = (int)Console.Read();
+            //Console.WriteLine(character);
+
+
+
+
+            //............................................................................
+            //a programe that takes 3 Numbers and show average with two digits
+            //............................................................................
+           // int firstNumber, secondNumber, thirdNumber;
+
+           // Console.Write("please input first Number: ");
+           // firstNumber = int.Parse(Console.ReadLine());
+
+           // Console.Write("please input second Number: ");
+           // secondNumber = int.Parse(Console.ReadLine());
+
+           // Console.Write("please input third Number: ");
+           // thirdNumber = int.Parse(Console.ReadLine());
+
+           //float ave = (float)(firstNumber + secondNumber + thirdNumber) / 3;
+           // Console.WriteLine("------------------------------------------");
+
+           // Console.WriteLine("{0:f2}",ave);
+        }
+    }
+}
